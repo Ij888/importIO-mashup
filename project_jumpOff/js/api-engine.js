a8902f0ea6e6befd877775983d18ba6092338ec4
Ij888 <ij.ofili@gmail.com>
2016-02-22 19:33:33 +0000

    added entry

--- project_jumpOff/js/api-engine.js
+++ project_jumpOff/js/api-engine.js
@@ -15,6 +15,23 @@ gapi.client.load('your_api_name', 'v1', function() {
 
 var payload = 'https://api.import.io/store/connector/3bcafba9-b5f3-4feb-a248-6265c0119606/_query?input=webpage/url:https%3A%2F%2Fwww.packtpub.com%2Fpackt%2Foffers%2Ffree-learning&&_apikey=81c93652057b496d886a97b5465b35816a2e3c29f89aeb50093e05ceef67b1fb244271b5156fdafb31f67b08e9eb8a3b99f12fc06f2d6eb1f86d081e206b852b63487fc7f22be6301831da2dda4142fe';
 
+var curl;
+(function () {
+
+	curl({
+		main: 'hello',
+		packages: {
+			// Your application's package
+			hello: { location: 'hello' },
+			// Third-party packages
+			curl: { location: 'lib/curl/src/curl' },
+			/*jquery: { location: 'lib/jquery/jquery', main: '.' },
+			Backbone: { location: 'lib/backbone-amd/backbone', main: '.' },
+			underscore: { location: 'lib/lodash/lodash', main: '.' }*/
+		}
+	});
+
+}());
 console.log(payload);
 
 
